0da3cee6ad8fd506b9ff93ff676d5265b1be2b8b
camilalonart <42299818+camilalonart@users.noreply.github.com>
2026-04-20 04:54:11 +0000

    Deploying to gh-pages from @ camilalonart/camilalonart@1315172311edba2f9a0b7463040029a1d84ecd44 🚀

--- _next/static/chunks/app/art/about/layout-dee782ebe3528cda.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8634,7409,5206,6705,4166,6493,20,1711,5154,4853,7632],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
