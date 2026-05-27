e291f09926cb7eb181073cb0298a29de27383fcb
camilalonart <42299818+camilalonart@users.noreply.github.com>
2026-05-27 15:50:07 +0000

    Deploying to gh-pages from @ camilalonart/camilalonart@918af6d2ec4caeeacd5556dae8d791fab0b3648e 🚀

--- _next/static/chunks/app/art/[collection]/[painting]/layout-82b430397a63b0cf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7632,7409,1998,5206,4166,1584,6705,8634,6493,20,1711,5154,4853],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
